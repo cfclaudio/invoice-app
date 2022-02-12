a2fad848f3764b4b9790a055e99fc0ace28c2296
cfclaudio <claudio.charmaine@gmail.com>
2022-02-12 13:45:12 -0500

    Pass mongoose connection options

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
 
     console.log(`MongoDB Connect: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
